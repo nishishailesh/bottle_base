69186ec71730c244ea0ad2d38be0c72266771f74
Your Name <you@example.com>
2024-01-06 00:10:38 +0530

    ok

--- views/check_login.tpl
+++
@@ -1,5 +0,0 @@
-<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-{{!post_table}}
-{{login_user}}
-{{message}}
-</form>
--- wsgi.py
+++ wsgi.py
@@ -3,10 +3,10 @@ import os, sys
 # Change working directory so relative paths (and template lookup) work again
 os.chdir(os.path.dirname(__file__))
 
-sys.path.append('/root/projects/clgp')
+sys.path.append('/root/projects/bottle')
 
 import bottle
-import clgp   #entry point
+import hello
 # ... build or import your bottle application here ...
 # Do NOT use bottle.run() with mod_wsgi
 application = bottle.default_app()
